959ccc5f167356739ad6393630d61b345db91295
Rishi Kumar <rsi.sxs@gmail.com>
2022-12-05 11:56:50 +0530

    add menu bar items in ui

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-
+//QApplication is going to handle event handling...mouse movements etc
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-
+//This is going to define all the functionality
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
+
 }
 
 MainWindow::~MainWindow()
